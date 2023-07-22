2c55554434b7a91e25fee75d868caa23f7ac4a52
Victor/Alyx Bersy <victor.bersy@hey.com>
2023-07-23 00:42:24 +0200

    Rework light theme + add dark mode (#4)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,6 @@
 // npx tailwindcss -i ./assets/src/css/main.css -o ./public/css/main.css --watch
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./views/**/*.html",
-    "./views/*.html",
-    "./assets/src/css/**.css",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+    content: ["./views/**/*.html", "./views/*.html", "./assets/src/css/**.css"],
+    plugins: [require("@tailwindcss/forms")],
+};
